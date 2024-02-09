ffd95fb2b8864f12fa2f8cf47078f49fe6b190f4
robert-szg <155324734+robert-szg@users.noreply.github.com>
2024-02-09 02:06:02 -0300

    Build all necessary functions to run the game

---
+++ javascript.js
@@ -0,0 +1,96 @@
+
+function getComputerChoice() {
+    let answer = Math.floor(Math.random() * 3);
+    
+    switch (answer) {
+        case 0:
+            return "rock"
+            break;
+        
+        case 1:
+            return "paper"
+            break;
+
+        case 2:
+            return "scissor"
+            break;
+
+        default:
+            return "ERROR"
+            break;
+    }
+};
+
+function playRound(playerSelection, computerSelection){
+
+    // playerSelection = playerSelection.toLowerCase();
+    console.log(`Player: ${playerSelection} || PC: ${computerSelection}`);
+
+    switch (playerSelection.toLowerCase()) {
+        case 'rock':
+            if(computerSelection === 'rock'){
+                return `It's a Draw! Computer also got ${computerSelection}`;
+            }
+            if(computerSelection === 'paper'){
+                return `You Lose! Computer got ${computerSelection}, which beats Rock`;
+            }
+            if(computerSelection === 'scissor'){
+                return `You Won! Computer got ${computerSelection}`;
+            }
+            break;
+    
+        case 'paper':
+            if(computerSelection === 'rock'){
+                return `You Won! Computer got ${computerSelection}`;
+            }
+            if(computerSelection === 'paper'){
+                return `It's a Draw! Computer also got ${computerSelection}`;
+            }
+            if(computerSelection === 'scissor'){
+                return `You Lose! Computer got ${computerSelection}, which beats Paper`;
+            }
+            break;
+            
+        case 'scissor':
+            if(computerSelection === 'rock'){
+                return `You Lose! Computer got ${computerSelection}, which beats Scissor`;
+            }
+            if(computerSelection === 'paper'){
+                return `You Won! Computer got ${computerSelection}`;
+            }
+            if(computerSelection === 'scissor'){
+                return `It's a Draw! Computer also got ${computerSelection}`;
+            }
+            break;
+
+        default:
+
+            break;
+    }
+}
+
+function playGame(){
+
+    let playerScore = 0, computerScore = 0;
+    let output = '';
+    let playerInput = '';
+    
+
+    for(let i = 0; i < 5; i++){
+        playerInput = prompt("Choose Rock, Paper or Scissor!");
+        console.log((output = playRound(playerInput, getComputerChoice())));
+
+    
+        if(output.startsWith("You Won!")) playerScore++;
+        if(output.startsWith("It's a Draw!")) continue;
+        if(output.startsWith("You Lose!")) computerScore++;
+    
+    }
+
+    if(playerScore > computerScore) return `Player Won the Game! Congratulations!`;
+    if(playerScore < computerScore) return `Computer Won the Game! Congratulations!`;
+    if(playerScore === computerScore) return `It's a Draw! Nobody Won the Game`;
+
+}
+
+console.log(playGame());
